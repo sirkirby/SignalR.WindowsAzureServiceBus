02ccd59f905ee1d3f8e8bc3ee0417ba91e0a7dc9
David Fowler <davidfowl@gmail.com>
2012-05-09 01:43:07 -0700

    Fixed version

--- SignalR.WindowsAzureServiceBus/Properties/AssemblyInfo.cs
+++ SignalR.WindowsAzureServiceBus/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.5.0.0")]
 [assembly: AssemblyFileVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0-pre")]
+[assembly: AssemblyInformationalVersion("0.5.0.0")]
 
