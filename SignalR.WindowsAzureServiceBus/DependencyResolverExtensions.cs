1fc59182ade18514a4683927dcd4a963ecf3c7f0
Chris Kirby <sirkirby@gmail.com>
2012-09-12 12:23:20 -0700

    removed azure service runtime
    adapted library to work outside of an azure web role instance. I remove
    the windows azure service runtime assembly and the associated code in
    the dependency resolver extension class. I replaced the code with a
    simple retrieval of the machines local ip address as the instance number
    in the GetRoleInstance method. Also updated all packages to their latest
    releases. Due to the service bus sdk update from 1.6 to 1.7, i adapted
    the code in the servicebusmessage bus to use the new
    'CreateFromConnectionString' methods as preferred by the documentation.
    Becuase of that I also changed the signature of the dependencey resolver
    extension methods.

--- SignalR.WindowsAzureServiceBus/DependencyResolverExtensions.cs
+++ SignalR.WindowsAzureServiceBus/DependencyResolverExtensions.cs
@@ -15,57 +15,48 @@
 //---------------------------------------------------------------------------------
 
 using System;
-using Microsoft.WindowsAzure.ServiceRuntime;
-using SignalR.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 
 namespace SignalR.WindowsAzureServiceBus
 {
-    public static class DependencyResolverExtensions
-    {
+	public static class DependencyResolverExtensions
+	{
+		public static IDependencyResolver UseWindowsAzureServiceBus(this IDependencyResolver resolver,
+																																string connectionStringName,
+		                                                            string topicPathPrefix,
+		                                                            int numberOfTopics)
+		{
+			var instanceId = Environment.MachineName + "_" + GetRoleInstanceNumber().ToString();
 
-        public static IDependencyResolver UseWindowsAzureServiceBus(this IDependencyResolver resolver,
-                                                                    string serviceBusNamespace,
-                                                                    string serviceBusAccount,
-                                                                    string serviceBusAccountKey,
-                                                                    string topicPathPrefix,
-                                                                    int numberOfTopics)
-        {
-            var instanceId = Environment.MachineName + "_" + GetRoleInstanceNumber().ToString();
+			return UseWindowsAzureServiceBus(resolver,
+																			 connectionStringName,
+			                                 topicPathPrefix,
+			                                 numberOfTopics,
+			                                 instanceId);
+		}
 
-            return UseWindowsAzureServiceBus(resolver, 
-                                             serviceBusNamespace, 
-                                             serviceBusAccount, 
-                                             serviceBusAccountKey, 
-                                             topicPathPrefix, 
-                                             numberOfTopics,
-                                             instanceId);
-        }
+		public static IDependencyResolver UseWindowsAzureServiceBus(this IDependencyResolver resolver,
+		                                                            string connectionStringName,
+		                                                            string topicPathPrefix,
+		                                                            int numberOfTopics,
+		                                                            string instanceId)
+		{
+			var bus = new Lazy<ServiceBusMessageBus>(() => new ServiceBusMessageBus(topicPathPrefix,
+			                                                                        numberOfTopics,
+			                                                                        connectionStringName,
+			                                                                        instanceId));
+			resolver.Register(typeof (IMessageBus), () => bus.Value);
+			return resolver;
+		}
 
-        public static IDependencyResolver UseWindowsAzureServiceBus(this IDependencyResolver resolver,
-                                                                    string serviceBusNamespace,
-                                                                    string serviceBusAccount,
-                                                                    string serviceBusAccountKey,
-                                                                    string topicPathPrefix,
-                                                                    int numberOfTopics,
-                                                                    string instanceId)
-        {
-            var bus = new Lazy<ServiceBusMessageBus>(() => new ServiceBusMessageBus(topicPathPrefix, 
-                                                                                    numberOfTopics, 
-                                                                                    serviceBusNamespace, 
-                                                                                    serviceBusAccount, 
-                                                                                    serviceBusAccountKey, 
-                                                                                    instanceId));
-            resolver.Register(typeof(IMessageBus), () => bus.Value);
-            return resolver;
-        }
-
-        private static int GetRoleInstanceNumber()
-        {
-            var roleInstanceId = RoleEnvironment.CurrentRoleInstance.Id;
-            var li1 = roleInstanceId.LastIndexOf(".");
-            var li2 = roleInstanceId.LastIndexOf("_");
-            var roleInstanceNo = roleInstanceId.Substring(Math.Max(li1, li2) + 1);
-            return Int32.Parse(roleInstanceNo);
-        }
-    }
+		private static Int64 GetRoleInstanceNumber()
+		{
+			// if not in an Azure web role, fall back to the server's unique local ip4 address
+			var host = Dns.GetHostEntry(Dns.GetHostName());
+			var address = host.AddressList.FirstOrDefault(ip => ip.AddressFamily == AddressFamily.InterNetwork);
+			return Int64.Parse(address.ToString().Replace(".", ""));
+		}
+	}
 }
